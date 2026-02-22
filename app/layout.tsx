bd945c8c84738d9ba7497b613a832d1405703f3c
haloandfable <tylerallen@live.com>
2026-02-21 19:22:07 -0500

    fix: use link.png as favicon for browser tab

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang='en' className='dark'>
       <head>
-        <link rel='icon' type='image/x-icon' href='/favicon.ico' />
+        <link rel='icon' type='image/png' href='/link.png' />
       </head>
       <body className='antialiased'>
         <Providers>{children}</Providers>
